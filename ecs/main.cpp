1edfdb49f9b33d40a0589d0f66f6662b39f9715b
Nick Field <heyimnick@live.com>
2021-03-25 21:48:19 +0000

    Initial Commit

---
+++ ecs/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ecs
+//
+//  Created by Nicholas Field on 25/03/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
